9ecf02183f0016830bc30922899496f3376d393d
hlia0024 <hlia0024@student.monash.edu>
2024-11-19 11:00:38 +0800

    Modify the login verification by checking the user name

--- rps/login.php
+++ rps/login.php
@@ -1,25 +1,31 @@
 <?php // Do not put any HTML above this line
 
-if ( isset($_POST['cancel'] ) ) {
-    // Redirect the browser to game.php
+if (isset($_POST['cancel'])) {
+    // Redirect the browser to index.php
     header("Location: index.php");
     return;
 }
 
 $salt = 'XyZzy12*_';
-$stored_hash = 'a8609e8d62c043243c4e201cbb342862';  // Pw is meow123
-
-$failure = false;  // If we have no POST data
+$stored_hash = 'a8609e8d62c043243c4e201cbb342862';  // Password is meow123
+$failure = false;  // To track error messages
 
 // Check to see if we have some POST data, if we do process it
-if ( isset($_POST['who']) && isset($_POST['pass']) ) {
-    if ( strlen($_POST['who']) < 1 || strlen($_POST['pass']) < 1 ) {
+if (isset($_POST['who']) && isset($_POST['pass'])) {
+    $username = $_POST['who'];
+    $password = $_POST['pass'];
+
+    // Check if fields are empty
+    if (strlen($username) < 1 || strlen($password) < 1) {
         $failure = "User name and password are required";
+    } elseif ($username !== 'XyZzy12*_') { // Check if username matches
+        $failure = "Access denied: Invalid username";
     } else {
-        $check = hash('md5', $salt.$_POST['pass']);
-        if ( $check == $stored_hash ) {
+        // Validate the password
+        $check = hash('md5', $salt . $password);
+        if ($check == $stored_hash) {
             // Redirect the browser to game.php
-            header("Location: game.php?name=".urlencode($_POST['who']));
+            header("Location: game.php?name=" . urlencode($username));
             return;
         } else {
             $failure = "Incorrect password";
@@ -39,11 +45,9 @@ if ( isset($_POST['who']) && isset($_POST['pass']) ) {
 <div class="container">
 <h1>Please Log In</h1>
 <?php
-// Note triple not equals and think how badly double
-// not equals would work here...
-if ( $failure !== false ) {
-    // Look closely at the use of single and double quotes
-    echo('<p style="color: red;">'.htmlentities($failure)."</p>\n");
+// Display error messages if any
+if ($failure !== false) {
+    echo('<p style="color: red;">' . htmlentities($failure) . "</p>\n");
 }
 ?>
 <form method="POST">
@@ -62,3 +66,4 @@ makes (all lower case) followed by 123. -->
 </p>
 </div>
 </body>
+</html>
